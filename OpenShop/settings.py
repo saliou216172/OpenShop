4a6faae3b4b39ea4eea4a663dff44670909daede
Mamadou Saliou BARRY <asbarry2468@gmail.com>
2025-10-29 19:24:43 +0000

    vrai changement

--- OpenShop/settings.py
+++ OpenShop/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url 
 
 # Build paths inside the project
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -52,10 +53,9 @@ WSGI_APPLICATION = 'OpenShop.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 # Password validation
