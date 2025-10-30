5c4da67f457a93b67fe872da19a5317687fa9ce9
Mamadou Saliou BARRY <asbarry2468@gmail.com>
2025-10-30 00:08:47 +0000

    voyons

--- OpenShop/settings.py
+++ OpenShop/settings.py
@@ -53,10 +53,12 @@ WSGI_APPLICATION = 'OpenShop.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
+    }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
