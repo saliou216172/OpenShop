9f246d0bf01e3c37c4b106cd1c5c1c9923a764a3
Mamadou Saliou BARRY <asbarry2468@gmail.com>
2025-11-04 03:20:05 +0000

    aussi

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from.import views 
+from . import views
 
-
-urlpatterns = [ 
-    path("", views.home, name="home"), 
+urlpatterns = [
+    path("", views.home, name="home"),
     path("creer_annonce/", views.creer_annonce, name="creer_annonce"),
-    path("categorie/<str:categorie_id>/", views.annonces_par_categorie, name='annonces_par_categorie'),
-    path('souscategorie/<int:souscategorie_id>/', views.annonces_par_souscategorie, name='annonces_par_souscategorie'),
-    path('get_sous_categories/', views.get_sous_categories, name='get_sous_categories'),
-    path('annonce/<int:annonce_id>/', views.detail_annonce, name='detail_annonce')
+    path("categorie/<int:categorie_id>/", views.annonces_par_categorie, name="annonces_par_categorie"),
+    path("souscategorie/<int:souscategorie_id>/", views.annonces_par_souscategorie, name="annonces_par_souscategorie"),
+    path("get_sous_categories/", views.get_sous_categories, name="get_sous_categories"),
+    path("annonce/<int:annonce_id>/", views.detail_annonce, name="detail_annonce"),
 ]
